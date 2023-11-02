341129b2f3970f64b8dfc1410119c53db7335e53
david reilly <davidreilly02@gmail.com>
2023-11-02 15:39:41 +0700

    Cleaned up a bit

--- my_selenium.py
+++ my_selenium.py
@@ -60,31 +60,23 @@ class Driver:
         sleep(5)
 
     def create_game_part_two(self, vocabs):
-        print('1')
-
         # Wait for the button to be clickable using the XPath
         image_library_button_xpath = "//div[@id='question-form']//button[@type='button']"
         image_library_button = WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, image_library_button_xpath))
         )
-
-        # Click the button
         image_library_button.click()
 
-        print('3')
         image_button = WebDriverWait(self.driver, 20).until(
             EC.element_to_be_clickable((By.ID, "web-lib"))
         )
         image_button.click()
-        print('4')
         sleep(6)
-
 
         close_button = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.CLASS_NAME, 'close-gif'))
         )
         close_button.click()
-        print('5')
         sleep(5)
         for val in vocabs:
             if val:
@@ -98,11 +90,6 @@ class Driver:
             EC.element_to_be_clickable((By.XPATH, close_game))
         )
         make_game.click()
-
-
-
-
-
 
     def close(self):
         self.driver.quit()
@@ -140,9 +127,6 @@ def questions_search_loop(self, vocabs):
         close_reopen(self)
         print('Had to close and reopen')
 
-
-
-
     save_button = WebDriverWait(self.driver, 10).until(
         EC.element_to_be_clickable((By.ID, "tally"))
     )
@@ -173,7 +157,6 @@ def close_reopen(self):
         print("Exception occurred while closing the popup: ", e)
 
 
-# driver = Driver()
 @app.route('/create_game', methods=['GET'])
 def create_game_route():
     data = session.get('data', {})
@@ -192,15 +175,12 @@ def create_game_route():
     try:
         local_driver.sign_in(url, EMAIL, PASSWORD)
         local_driver.create_game(title)
-        print('hi')
         local_driver.create_game_part_two(vocabs)
         return redirect(url_for('success_page'))
     except Exception as e:
         print(e)  # For debugging purposes, print the exception
         # Handle login failure, perhaps by redirecting to a login failure page
         return redirect(url_for('failure'))
-
-
 
 @app.route('/success', methods=['GET'])
 def success_page():
@@ -212,8 +192,6 @@ def failure():
 
 url = 'https://www.baamboozle.com/games/create'
 
-
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)  # Try a different port like 5001
+    app.run(debug=True, port=5001)
 
-#got as far as for loop doesnt seem to work yet. Check why
